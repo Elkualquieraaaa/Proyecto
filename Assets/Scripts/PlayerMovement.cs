f917e721dbe0243f9f2f1d98f5fac2a59efbc8f7
B0C4S2006 <sebastian.2006.bc@gmail.com>
2025-04-02 17:52:00 -0500

    player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Transform Player;
+    Rigidbody2D Rigidbody;
+    float Hmovement;
+    float Vmovement;
     // Start is called before the first frame update
     void Start()
     {
         Player = GetComponent<Transform>(); 
+        Rigidbody = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Hmovement = Input.GetAxis("Horizontal");
+        Vmovement = Input.GetAxis("Vertical");
 
+        Move();
+    }
 
     public void Move()
     {
-
+        Rigidbody.velocity = new Vector2(Hmovement,Vmovement);
     }
 }
