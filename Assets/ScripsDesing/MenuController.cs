3ae5563cd83c3f869dfc2589195c3711f507df6b
andresalejandroarrieta <castillorodriguezallisonnicole@gmail.com>
2025-04-02 17:39:16 -0500

    cambios jeje

--- Assets/ScripsDesing/MenuController.cs
+++ Assets/ScripsDesing/MenuController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(2);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
