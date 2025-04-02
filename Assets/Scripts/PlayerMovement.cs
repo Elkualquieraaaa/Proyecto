2040c8e51e8a7bf0102662bbeea25ef6137ff19f
B0C4S2006 <sebastian.2006.bc@gmail.com>
2025-04-02 17:41:14 -0500

    mas cambios

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Move()
+    {
+
+    }
+}
