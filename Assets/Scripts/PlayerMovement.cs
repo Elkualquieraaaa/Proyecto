99369bfb25eb9a17a2304254fa4a31ee89444c68
Elkualquieraaaa <triplehak88@gmail.com>
2025-04-07 00:10:21 -0500

    charactermovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Transform Player;
     Rigidbody2D Rigidbody;
+    [SerializeField] Transform PlayerDetector;
     float Hmovement;
     float Vmovement;
+    [SerializeField] float radius;
+    [SerializeField] float velocity;
+    float normalvelocity;
     // Start is called before the first frame update
     void Start()
     {
-        Player = GetComponent<Transform>(); 
-        Rigidbody = GetComponent<Rigidbody2D>();  
+        Rigidbody = GetComponent<Rigidbody2D>();
+        normalvelocity = velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Hmovement = (Input.GetAxis("Horizontal")*Time.deltaTime);
-        Vmovement = (Input.GetAxis("Vertical")*Time.deltaTime);
+        Physics2D.OverlapCircle(PlayerDetector.position, radius);
+
+        Hmovement = (Input.GetAxis("Horizontal") * velocity);
+        Vmovement = (Input.GetAxis("Vertical") * velocity);
 
         Move();
+        Run();
+        Hide();
     }
 
     public void Move()
     {
-        Rigidbody.velocity = new Vector2(Hmovement,Vmovement);
+        Rigidbody.velocity = new Vector2(Hmovement * Time.deltaTime, Vmovement * Time.deltaTime);
+    }
+
+    public void Run()
+    {
+        float newvelocity = (velocity / 1.2f + velocity);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            velocity = newvelocity;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            velocity = normalvelocity;
+        }
+    }
+
+    public void Hide()
+    {
+        bool hidden = false;
+        bool hiddent = true;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            gameObject.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            gameObject.SetActive(true);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PlayerDetector.position, radius);
     }
 }
