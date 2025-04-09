7a9ff3f66275d7f9efa8703f0722a40d1e5b5da7
Lucplash <luiscarlos.lcph@gmail.com>
2025-04-09 13:56:31 -0500

    Seguimiento de player

--- Assets/Scripts/PoliceMovement.cs
+++ Assets/Scripts/PoliceMovement.cs
@@ -1,50 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class PenduloEnemigo : MonoBehaviour
 {
-    [SerializeField] Transform target1, target2; 
+    [SerializeField] Transform target1, target2;
     private Transform currentTarget;
     public float Speed;
 
-    public float detectionRadius; 
-    public LayerMask playerLayer; 
+    public float detectionRadius;
+    public LayerMask playerLayer;
     private Transform player;
 
-    public string gameOverSceneName = "MainMenu"; 
+    public string gameOverSceneName = "MainMenu";
 
     void Start()
     {
-        currentTarget = target1; 
+        currentTarget = target1;
     }
 
     void Update()
     {
-        
-        Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius, playerLayer);
+        // Detectar al jugador usando física 2D
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius, playerLayer);
 
         if (hits.Length > 0)
         {
-            
+            // Si hay colisión, apuntar al jugador
             player = hits[0].transform;
         }
         else
         {
-            
+            // Si no hay colisión, deshabilitar seguimiento al jugador
             player = null;
         }
 
-        
+        // Movimiento del enemigo
         if (player != null)
         {
-            
+            // Seguir al jugador si está detectado
             transform.position = Vector3.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
         }
         else
         {
-            
+            // Moverse entre puntos (target1 y target2) si el jugador no está cerca
             transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, Speed * Time.deltaTime);
 
             if (transform.position == currentTarget.position)
@@ -58,14 +58,14 @@ public class PenduloEnemigo : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            // Cambiar a la escena de Game Over
             SceneManager.LoadScene("MainMenu");
         }
     }
 
-
     void OnDrawGizmosSelected()
     {
-        
+        // Visualizar el radio de detección
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
