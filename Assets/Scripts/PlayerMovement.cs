a1ff3de520dd6af8856c6d85cbfd7c137276725e
B0C4S2006 <sebastian.2006.bc@gmail.com>
2025-04-09 14:30:33 -0500

    charactercontroller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,30 +6,43 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D Rigidbody;
+    SpriteRenderer spriteRenderer;
     [SerializeField] Transform PlayerDetector;
     float Hmovement;
     float Vmovement;
     [SerializeField] float radius;
     [SerializeField] float velocity;
     float normalvelocity;
-    Collider2D hideout;
+    [SerializeField] Collider2D hideout;
+    [SerializeField]bool hide;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] LayerMask playermask;
+    [SerializeField] GameObject player;
+    [SerializeField] Collider2D colider;
     // Start is called before the first frame update
+
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody2D>();
         normalvelocity = velocity;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = GetComponent<GameObject>();
+        colider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        hideout = Physics2D.OverlapCircle(PlayerDetector.position, radius);
+        hideout = Physics2D.OverlapCircle(PlayerDetector.position, radius,layerMask);
 
         Hmovement = (Input.GetAxis("Horizontal") * velocity);
         Vmovement = (Input.GetAxis("Vertical") * velocity);
-
-        Move();
+        if (hide != true)
+        {
+            Move();
+        }
         Run();
+        Hide();
     }
 
     public void Move()
@@ -48,6 +61,27 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             velocity = normalvelocity;
+        }
+    }
+    public void Hide()
+    {
+        if (hideout != null && Input.GetKeyDown(KeyCode.E))
+        {
+            spriteRenderer.enabled = false;
+            hide = true;
+            Rigidbody.velocity = new Vector2 (0, 0);
+            gameObject.layer = LayerMask.GetMask("default");
+            colider.enabled = false;
+            Debug.Log(gameObject.layer);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            spriteRenderer.enabled = true;
+            hide = false;
+            gameObject.layer = playermask;
+            colider.enabled = true;
+            Debug.Log(gameObject.layer);
         }
     }
     private void OnDrawGizmos()
