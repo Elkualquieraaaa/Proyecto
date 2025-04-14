cc2de450138b302753c5412c77d627da7b949d0c
allicast <castillorodriguezallisonnicole@gmail.com>
2025-04-13 19:54:31 -0500

    pause menu

---
+++ Assets/ScripsDesing/PauseManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject settingsPausePanel;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+        settingsPausePanel.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsPause()
+    {
+        pausePanel.SetActive(false);
+        settingsPausePanel.SetActive(true);
+    }
+}
+
